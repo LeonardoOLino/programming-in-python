8aff73440d970034e61aeba0e35d66c23de29852
LeonardoOLino <leonardo.linopm@gmail.com>
2026-01-31 15:49:15 -0300

    Add files via upload
    Signed-off-by: LeonardoOLino <leonardo.linopm@gmail.com>

---
+++ AULA_4.py
@@ -0,0 +1,27 @@
+## ATIVIDADES:
+## 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+numero = 10
+print(numero **2)
+
+## 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+nome = 'Leonardo'
+sobrenome = 'Lino'
+
+print(nome, sobrenome)
+
+## 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+n1 = int(input ("digite um numero:"))
+n2 = int(input ("digite um numero:"))
+
+print(n1, n2)
+
+## 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+name = 'Python'
+
+print (name, "20")
+
+## 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+frase = 'o Leleo e charmoso e'
+palavra = input('Digite uma palavra: ')
+
+print (frase, palavra)
